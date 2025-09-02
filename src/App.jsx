b20b3b55289da6391433df18b4c9e0da4a23a553
jefersonssant <jefss@outlook.com>
2025-09-02 16:17:14 -0300

    :white_check_mark: ci: Integração com API, requisição de dados

--- src/App.jsx
+++ src/App.jsx
@@ -2,57 +2,25 @@ import Container from "./components/container/Container"
 import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
 import "./globalStyle.scss"
-import { useState } from "react"
+import { useState, useEffect, use } from "react"
+import axios from "axios"
 
 function App() {
 
-    const [listaFilmes, setListaFilmes] = useState([
-    {
-      id: 1, imagem: "https://i.pinimg.com/736x/9f/a7/c5/9fa7c57b46d67a795c19b94bb16ce893.jpg",
-      banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRzctWUJ0A-a18WuGLoGm7kz3PlcGHZit8KA&s",
-      titulo: "O Grito"
-    },
-    {
-      id: 2, imagem: "https://i.pinimg.com/736x/9f/a7/c5/9fa7c57b46d67a795c19b94bb16ce893.jpg",
-      banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRzctWUJ0A-a18WuGLoGm7kz3PlcGHZit8KA&s",
-      titulo: "Halloween Ends"
-    },
-    {
-      id: 4, imagem: "https://i.pinimg.com/736x/9f/a7/c5/9fa7c57b46d67a795c19b94bb16ce893.jpg",
-      banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRzctWUJ0A-a18WuGLoGm7kz3PlcGHZit8KA&s",
-      titulo: "Pânico 5"
-    },
-    {
-      id: 5, imagem: "https://i.pinimg.com/736x/9f/a7/c5/9fa7c57b46d67a795c19b94bb16ce893.jpg",
-      banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRzctWUJ0A-a18WuGLoGm7kz3PlcGHZit8KA&s",
-      titulo: "Pânico 5"
-    },
-    {
-      id: 6, imagem: "https://i.pinimg.com/736x/9f/a7/c5/9fa7c57b46d67a795c19b94bb16ce893.jpg",
-      banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRzctWUJ0A-a18WuGLoGm7kz3PlcGHZit8KA&s",
-      titulo: "Pânico 5"
-    },
-    {
-      id: 7, imagem: "https://i.pinimg.com/736x/9f/a7/c5/9fa7c57b46d67a795c19b94bb16ce893.jpg",
-      banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRzctWUJ0A-a18WuGLoGm7kz3PlcGHZit8KA&s",
-      titulo: "Pânico 5"
-    },
-    {
-      id: 8, imagem: "https://i.pinimg.com/736x/9f/a7/c5/9fa7c57b46d67a795c19b94bb16ce893.jpg",
-      banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRzctWUJ0A-a18WuGLoGm7kz3PlcGHZit8KA&s",
-      titulo: "Pânico 5"
-    },
-    {
-      id: 9, imagem: "https://i.pinimg.com/736x/9f/a7/c5/9fa7c57b46d67a795c19b94bb16ce893.jpg",
-      banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRzctWUJ0A-a18WuGLoGm7kz3PlcGHZit8KA&s",
-      titulo: "Pânico 5"
-    },
-    {
-      id: 10, imagem: "https://i.pinimg.com/736x/9f/a7/c5/9fa7c57b46d67a795c19b94bb16ce893.jpg",
-      banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRzctWUJ0A-a18WuGLoGm7kz3PlcGHZit8KA&s",
-      titulo: "Pânico 5"
+  const getFilmes = async () => {
+    try {
+      const responsta = await axios.get("https://api-streamflix.onrender.com/api/filmes")
+      setListaFilmes(responsta.data)
+      console.log(responsta.data)
+    } catch (error) {
+      console.log("Erro ao conectar com o banco de dados: ", error)
     }
-  ])
+  }
+  useEffect(() => {
+    getFilmes()
+  }, [])
+
+  const [listaFilmes, setListaFilmes] = useState([])
   const [assistindo, setAssistindo] = useState([])
   const [favoritoMinhaLista, setFavoritoMinhaLista] = useState([])
 
